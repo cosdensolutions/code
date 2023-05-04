aca0bf1cdcfebdf1b993d06229f27167165b4162
Darius Cosden <darius@cosdensolutions.io>
2023-05-04 14:31:03 +0200

    master: added useEffect tutorial source code

---
+++ videos/long/learn-react-hooks-useEffect/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+interface DemoProps {}
+
+export default function Demo({}: DemoProps) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // The code that we want to run
+    console.log('The count is:', count);
+
+    // Optional return function
+    return () => {
+      console.log('I am being cleaned up!');
+    };
+  }, [count]); // The dependency array
+
+  return (
+    <div className='tutorial'>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
