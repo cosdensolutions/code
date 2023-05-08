04ec50223cc81af1d71cff3e9c573cd6817030e9
Darius Cosden <darius@cosdensolutions.io>
2023-05-08 17:38:28 +0200

    master: added useMemo tutorial source code

---
+++ videos/long/learn-react-hooks-useMemo/index.tsx
@@ -0,0 +1,27 @@
+import { useMemo, useState } from 'react';
+
+import { initialItems } from './utils';
+
+interface DemoProps {}
+
+function Demo({}: DemoProps) {
+  const [count, setCount] = useState(0);
+  const [items] = useState(initialItems);
+
+  const selectedItem = useMemo(
+    () => items.find((item) => item.id === count),
+    [count, items],
+  );
+
+  return (
+    <div className='tutorial'>
+      <h1>Count: {count}</h1>
+      <h1>Selected Item: {selectedItem?.id}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default Demo;
---
+++ videos/long/learn-react-hooks-useMemo/utils.tsx
@@ -0,0 +1,6 @@
+export const initialItems = new Array(29_999_999).fill(0).map((_, i) => {
+  return {
+    id: i,
+    isSelected: i === 29_999_998,
+  };
+});
