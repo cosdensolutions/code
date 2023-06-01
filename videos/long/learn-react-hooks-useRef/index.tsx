c859253972811e9b5ffae1e30c1794c8d08478db
Darius Cosden <darius@cosdensolutions.io>
2023-06-01 22:51:53 +0200

    added useRef tutorial source code

---
+++ videos/long/learn-react-hooks-useRef/index.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+
+interface DemoProps {}
+
+export default function Demo({}: DemoProps) {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+    countRef.current++;
+
+    console.log("State:", count);
+    console.log("Ref:", countRef.current);
+  };
+
+  return (
+    <div className="tutorial">
+      Count: {count}
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+}
+
+export function Demo2({}: DemoProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  return (
+    <div className="tutorial">
+      <input ref={inputRef} type="text" placeholder="Type something..." />
+    </div>
+  );
+}
